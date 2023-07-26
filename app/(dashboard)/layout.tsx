ff639eccc17a412683c824b014959b0b6fb001e9
satyam <satyamsonker1010@gmail.com>
2023-07-27 02:55:22 +0530

    add some foe code like creare a side bard and home page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <div className="text-cyan-50">Hello Sidebar</div>
+        <Sidebar />
       </div>
       {/* width of sidebar and padding left of main tag is same */}
       <main className="md:pl-72">
+        <Navbar />
         {children}
       </main>
     </div>
